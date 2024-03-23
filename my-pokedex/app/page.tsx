68f115839af0cb916458ff95dd503ebc30dc034d
jhaes <julie.haesendonckx@hotmail.com>
2024-03-23 21:34:18 +0100

    Update the home page

--- my-pokedex/app/page.tsx
+++ my-pokedex/app/page.tsx
@@ -11,30 +11,30 @@ export default function Home() {
         <div className='col-3'>
           <div className='card' style={{ width: '18rem' }}>
             <Image className='card-img-top mt-2'
-              src="/img/grass.png"
+              src="/img/Pikachu.jpg"
               width={150}
               height={150}
               alt="Grass Energy"
             />
             <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
-              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link href="#" className='btn btn-success'>Go Somewhere</Link>
+              <h5 className='card-title'>Pokémon</h5>
+              <p className='card-text'>Scroll through all the pokémon in the Kanto region Pokédex.</p>
+              <Link href="/pokemon" className='btn btn-success'>Go Somewhere</Link>
             </div>
           </div>
         </div>
         <div className='col-3'>
           <div className='card' style={{ width: '18rem' }}>
             <Image className='card-img-top mt-2'
-              src="/img/electric.png"
+              src="/img/AshBadge.png"
               width={150}
               height={150}
-              alt="Electric Energy"
+              alt="Ash holding a badge."
             />
             <div className='card-body'>
-              <h5 className='card-title'>Pokémons</h5>
-              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link href="/pokemon" className='btn btn-success'>Go Somewhere</Link>
+              <h5 className='card-title'>Badges</h5>
+              <p className='card-text'>Overview of all the Kanto-region badges grouped per gym. See which gym leader must be defeated to win each badge.</p>
+              <Link href="/badges" className='btn btn-success'>Go Somewhere</Link>
             </div>
           </div>
         </div>
@@ -47,9 +47,9 @@ export default function Home() {
               alt="Fire Energy"
             />
             <div className='card-body'>
-              <h5 className='card-title'>Gyms</h5>
-              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <Link href="#" className='btn btn-success'>Go Somewhere</Link>
+              <h5 className='card-title'>Energy Types</h5>
+              <p className='card-text'>The type of a pokémon determines its strenghts and weaknesses.</p>
+              <Link href="/energy" className='btn btn-success'>Go Somewhere</Link>
             </div>
           </div>
         </div>
